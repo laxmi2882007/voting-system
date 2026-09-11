17386c375fa9b58eef15af76c2f05c07820906a0
laxmi2882007 <l3655648@gmail.com>
2026-09-11 16:25:03 +0530

    Update voter eligibility message for age requirement

--- vote.py
+++ vote.py
@@ -5,4 +5,4 @@ if age >= 18:
     print("you can register as a voter.")
 else:
     print("you are not eligible to vote.")
-    print("you can not register as a voter.")
+    print("you must be at least 18 years old.")
