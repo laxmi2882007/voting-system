78d4eb068061cd3c52b8aa00c6c0e4154f467793
laxmi2882007 <l3655648@gmail.com>
2026-09-11 14:36:37 +0530

    Add voter registration message for eligible users

--- vote.py
+++ vote.py
@@ -2,5 +2,7 @@ age = int(input("Enter your age:"))
 
 if age >= 18:
     print("you are eligible to vote.")
+    print("you can register as a voter.")
 else:
     print("you are not eligible to vote.")
+    print("you must be at least 18 years old.")
